996f878d3417ded6ddc899733eada71be72024a4
Tyler Cobb <Tyler.Cobb@ties.k12.mn.us>
2017-04-28 06:46:53 -0500

    Added some logging and took advantage of len() evaluating true if != 0

--- ConsecutiveRuns.py
+++ ConsecutiveRuns.py
@@ -3,7 +3,19 @@ class ConsecutiveRunParser(object):
 	
 	@staticmethod
 	def findRuns(numbers, length=3, noResultsFoundText="No Runs Found"):
+		"""
+		Takes in a list of numbers and searchs for runs of specified length.
+		A run is a defined as a series of numbers that all increment or 
+		decrement by 1.
+		@param numbers, the list of numbers to process
+		@param length, the required length of the run
+		@param noResultsFoundText, what the output will be if no runs are found
+		"""
 		def isRun(subset):
+			"""
+			Helper function to determine if a specified subset is a run or not
+			@param subset, a subset of the list of numbers provided to findRuns
+			"""
 			if len(subset) != length:
 				return False
 				
@@ -11,7 +23,7 @@ class ConsecutiveRunParser(object):
 			return len(set(diffs)) == 1 and diffs[0] in ConsecutiveRunParser.VALID_RUN_DIFFS
 			
 		indecies = [index for index in range(len(numbers)) if isRun(numbers[index:index+length])]
-		return indecies if len(indecies) != 0 else noResultsFoundText
+		return indecies if len(indecies) else noResultsFoundText
 		
 def runTest(input, expected):
 	indecies = ConsecutiveRunParser.findRuns(input)
@@ -26,7 +38,7 @@ def runTests():
 	runTest([1,2], "No Runs Found")
 	runTest([-1,-2,-3], [0])
 	runTest([0,0,0], "No Runs Found")
-	runTest([0,2,0,2], "No Runs Found")
+	runTest([0,2,4,6], "No Runs Found")
 	runTest([0,1,2,3,4,5,6,7], [0,1,2,3,4,5])
 	runTest([7,6,5,4,3,2,1,0], [0,1,2,3,4,5])
 			
