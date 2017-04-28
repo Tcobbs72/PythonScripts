c5ef6b6afdb072053a83451ca10835a3ff4494b4
Tyler Cobb <Tyler.Cobb@ties.k12.mn.us>
2017-04-28 07:11:02 -0500

    Removed unused code

--- find_consecutive_runs.py
+++ find_consecutive_runs.py
@@ -41,13 +41,6 @@ def runTests():
 	runTest([0,2,4,6], "No Runs Found")
 	runTest([0,1,2,3,4,5,6,7], [0,1,2,3,4,5])
 	runTest([7,6,5,4,3,2,1,0], [0,1,2,3,4,5])
-	
-def isDigit(num):
-	try:
-		int(num)
-		return True
-	except ValueError:
-		return False
 			
 if __name__ == '__main__':
 	import sys
